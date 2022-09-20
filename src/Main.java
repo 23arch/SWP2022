01f3aeabc5447f5e9d20547edf7ad82c6c4e6e19
talha <talha.yuecel@student.htldornbirn.at>
2022-09-20 16:03:43 +0200

    St

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+
+        //short - ganze Zahl
+        //int - ganze Zahl
+        //float - Kommazahl
+        //double -- Kommazahl (groß)
+        //boolean - true/false
+        //char - Zeichen
+    }
+    //strg blank
+}
