89388d54a6c3314be4b75947f9dd7bac8df18426
talha <talha.yuecel@student.htldornbirn.at>
2022-09-21 11:50:20 +0200

    Code teilweise fertig

--- src/at/ran/basics/Control.java
+++ src/at/ran/basics/Control.java
@@ -9,10 +9,20 @@ public class ControlStructurs3 {
         int randomNumber = random.nextInt(100);
 
         // Gib die Zufallszahl aus
+        System.out.println(randomNumber);
 
-        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+        // Wenn die Zahl kleiner ist als 20 gib aus "Mini"
+
+        if (randomNumber < 20) {
+            System.out.println("Mini");
+        }
         // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
         // Wenn die Zahl größer als 50 ist gib aus "Large"
+        else (randomNumber > 50);
+        {
+            System.out.println("Large");
+        }
+        ;
 
 
     }
