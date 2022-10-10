f98eb69bf0383d4b7b0f907a30916ec0828ff4e3
talha <talha.yuecel@student.htldornbirn.at>
2022-10-10 13:04:09 +0200

    Bankomat

--- src/at/ran/basics/Bankomatbeispiel.java
+++ src/at/ran/basics/Bankomatbeispiel.java
@@ -1,8 +1,41 @@
 package at.ran.basics;
 
+import java.util.Scanner;
 public class Bankomatbeispiel {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int Kontostand = 0;
+        System.out.println("Wählen Sie eine Funktion");
+        System.out.println("1. Einzahlen");
+        System.out.println("2. Abheben");
+        System.out.println("3. Kontostand");
+        System.out.println("4. Abbruch");
+        boolean finish = false;
+        int selection = 0;
+        while (!finish) {
+            int Selection = scan.nextInt();
+        }
+        if (selection == 1) {
+            System.out.println("Geben sie den Betrag ein, den Sie einzahlen möchten");
+            int Einzahlung = scan.nextInt();
 
+            Kontostand = Kontostand + Einzahlung;
+            System.out.println("Sie haben " + Einzahlung + "€ eingezahlt!");
+        }
+        if (selection == 2) {
+            System.out.println("Wie viel € wollen Sie abheben?");
+            int Auszahlung = scan.nextInt();
+            Kontostand = Kontostand - Auszahlung;
+            System.out.println("Sie haben " + Auszahlung + "€ abgehoben!");
+        }
+        if (selection == 3) {
+            System.out.println("Der Kontostand beträgt:" + Kontostand + "€");
+        }
+
+        if (selection == 4) {
+            finish = true;
+            System.out.println("Vorgang abgebrochen!");
+        }
     }
 }
 
