fb9bc78dd3bd84e904beb80c93e221d04574a9fd
talha <talha.yuecel@student.htldornbirn.at>
2022-10-17 11:47:44 +0200

    n

--- src/at/ran/basics/Bankomatbeispiel.java
+++ src/at/ran/basics/Bankomatbeispiel.java
@@ -1,6 +1,7 @@
 package at.ran.basics;
 
 import java.util.Scanner;
+
 public class Bankomatbeispiel {
     public static void main(String[] args) {
         boolean finish = false;
@@ -13,28 +14,34 @@ public class Bankomatbeispiel {
         Scanner scan = new Scanner(System.in);
         int selection = scan.nextInt();
         while (!finish) {
-            int Selection = scan.nextInt();
-        }
-        if (selection == 1) {
-            System.out.println("Geben sie den Betrag ein, den Sie einzahlen möchten");
-            int Einzahlung = scan.nextInt();
+            System.out.println("Wählen Sie eine Funktion");
+            System.out.println("1. Einzahlen");
+            System.out.println("2. Abheben");
+            System.out.println("3. Kontostand");
+            System.out.println("4. Abbruch");
+            selection = scan.nextInt();
 
-            Kontostand = Kontostand + Einzahlung;
-            System.out.println("Sie haben " + Einzahlung + "€ eingezahlt!");
-        }
-        if (selection == 2) {
-            System.out.println("Wie viel € wollen Sie abheben?");
-            int Auszahlung = scan.nextInt();
-            Kontostand = Kontostand - Auszahlung;
-            System.out.println("Sie haben " + Auszahlung + "€ abgehoben!");
-        }
-        if (selection == 3) {
-            System.out.println("Der Kontostand beträgt:" + Kontostand + "€");
-        }
+            if (selection == 1) {
+                System.out.println("Geben sie den Betrag ein, den Sie einzahlen möchten");
+                int Einzahlung = scan.nextInt();
 
-        if (selection == 4) {
-            finish = true;
-            System.out.println("Vorgang abgebrochen!");
+                Kontostand = Kontostand + Einzahlung;
+                System.out.println("Sie haben " + Einzahlung + "€ eingezahlt!");
+            }
+            if (selection == 2) {
+                System.out.println("Wie viel € wollen Sie abheben?");
+                int Auszahlung = scan.nextInt();
+                Kontostand = Kontostand - Auszahlung;
+                System.out.println("Sie haben " + Auszahlung + "€ abgehoben!");
+            }
+            if (selection == 3) {
+                System.out.println("Der Kontostand beträgt:" + Kontostand + "€");
+            }
+
+            if (selection == 4) {
+                finish = true;
+                System.out.println("Vorgang abgebrochen!");
+            }
         }
     }
 }
