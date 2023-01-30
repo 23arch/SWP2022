0251afdd5118964a5d4719193c73f20c7775e2f9
unknown <talha.yuecel@student.htldornbirn.at>
2023-01-30 12:23:42 +0100

    In Cars a ausgegeben

--- src/at/ran/OO/Remote/Main.java
+++ src/at/ran/OO/Remote/Main.java
@@ -1,9 +1,12 @@
 package at.ran.OO.Remote;
 
-public class Main
-{
+public class Main {
 
-    Batterie b1 = new Batterie(90);
-    Batterie b2 = new Batterie (80);
-    Remote r1 = new Remote(b1, b2);
+    public static void main(String[] args) {
+        Batterie b1 = new Batterie(90);
+        Batterie b2 = new Batterie(80);
+        Remote r1 = new Remote(b1, b2);
+
+        System.out.println("a");
+    }
 }
